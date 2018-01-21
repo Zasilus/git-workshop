eb1007bbfd608918d188b10ceaba81a1b246b0e6
Zixstar <asg101@case.edu>
2018-01-21 14:30:39 -0500

    changed fizzbuzz to start from 1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 def fizzbuzz(n):
-    for i in range(n):
+    for i in range(1, n + 1):
         if i % 15 == 0:
-            print "fizzbuzz"
+            print ("fizzbuzz")
         elif i % 3 == 0:
-            print "fizz"
+            print ("fizz")
         elif i % 5 == 0:
-            print "buzz"
+            print ("buzz")
         else:
-            print i
+            print (i)
